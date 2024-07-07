1f70e9ec40b3d711609da2761a47c5cf0b8b0444
nuresrasoylu <nuresrasoylu@gmail.com>
2024-07-07 19:33:15 +0200

    Description is fixed in URL SQLi Auxiliary

--- esra_sql_injection_url.rb
+++ esra_sql_injection_url.rb
@@ -17,7 +17,7 @@ class MetasploitModule < Msf::Auxiliary
                       'Description' => '
         This module demonstrates a basic SQL injection vulnerability exploit on a target web application.
         It is intended for beginner-level users to understand the principles of SQL injection attacks.
-        The module sends a crafted SQL query via the User-Agent HTTP header and checks if the target is
+        The module sends a crafted SQL query via the URL and checks if the target is
         vulnerable by analyzing the server response. If the target is found to be vulnerable, the module
         confirms the exploit and reports the vulnerability.
       ',
